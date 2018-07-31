1f30242c5e544c7128f072c2c4532ee6dc37a644
Krzysztof Starkowicz <38637017+Bysiek5@users.noreply.github.com>
2018-07-31 16:50:00 +0200

    Add files via upload

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { HttpClientModule } from '@angular/common/http';
 
 const appRoutes: Routes = [
     {path: '', component: HomeComponent },
@@ -20,7 +21,8 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
